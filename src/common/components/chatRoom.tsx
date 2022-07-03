371ff80267b86c27bec32567e3cca9705968dd32
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2022-07-03 14:42:52 +0700

    make fetch comment

--- src/common/components/chatRoom.tsx
+++ src/common/components/chatRoom.tsx
@@ -8,6 +8,7 @@ import { TbLollipop } from "react-icons/tb";
 import { BaseLayout, BasePageHeader } from "../../common/components";
 import relativeTime from "dayjs/plugin/relativeTime";
 import PostService from "../service/posts";
+import CommentService from "../service/comment";
 
 dayjs.extend(relativeTime);
 
@@ -17,13 +18,16 @@ interface IChatRoom {
 
 const ChatRoom: React.FC<IChatRoom> = ({ username }) => {
   const [post, setPostData] = useState<any>({});
-  //const [comment, setCommentData] = useState<any>({});
+  const [user, SetUserData] = useState<any>({});
+  const [comment, setCommentData] = useState<any>([]);
   const Router = useRouter();
+
+  let commentArray: any = [];
 
   const featchData = async () => {
     const res = await PostService.id("1");
     setPostData(res);
-    console.log(res.description);
+    SetUserData(res.user);
   };
 
   useEffect(() => {
@@ -32,28 +36,28 @@ const ChatRoom: React.FC<IChatRoom> = ({ username }) => {
       Router.push("/login");
     }
     featchData();
-    console.log(post.user);
-  }, []); 
+  }, []);
 
   const onFinish = (values: any) => {
     console.log("Success:", values);
     // TODO: API HERE
-
-    // AuthService.login(values)
-    //   .then((data) => {
-    //     console.log(data);
-    //     localStorage.setItem("token", data.access_token);
-    //     localStorage.setItem("user", JSON.stringify(data.user));
-    //     Router.push("/");
-    //   })
-    //   .catch((e) => console.log(e));
+    CommentService.send(values, post.id)
+      .then(async (data) => {
+        console.log(data);
+        await CommentService.list(post.id).then((data) => {
+          setCommentData(data);
+          // commentArray = data;
+          // console.log(commentArray);
+        });
+      })
+      .catch((e) => console.log(e));
   };
 
   return (
     <BaseLayout>
       <div className="h-full min-h-[calc(100vh-56px)] flex flex-col ">
         <BasePageHeader
-          title={post.title}
+          title={user.username + "'s post"}
           handleBack={() => Router.back()}
           handleAction={() => {}}
           action={<BiDotsVerticalRounded className="h-6 w-6" />}
@@ -69,16 +73,14 @@ const ChatRoom: React.FC<IChatRoom> = ({ username }) => {
                 />
               </div>
               <div className="flex-1">
-                <p className="m-0 font-medium text-sm"></p>
+                <p className="m-0 font-medium text-sm">{user.username}</p>
                 <p className="m-0 text-zinc-400 text-xs">1.2 km far from you</p>
               </div>
               <span className="flex items-start text-zinc-400 text-xs">
                 48 min
               </span>
             </div>
-            <div className="">
-              {post.description}
-            </div>
+            <div className="">{post.description}</div>
             <div className="flex space-x-4">
               <div className="flex space-x-2 items-center">
                 <TbLollipop className="text-zinc-500" />
@@ -86,24 +88,26 @@ const ChatRoom: React.FC<IChatRoom> = ({ username }) => {
               </div>
             </div>
           </div>
-          <Comment
-            author={<a>Han Solo</a>}
-            avatar={
-              <Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
-            }
-            content={
-              <p>
-                We supply a series of design principles, practical patterns and
-                high quality design resources (Sketch and Axure), to help people
-                create their product prototypes beautifully and efficiently.
-              </p>
-            }
-            datetime={<span>{dayjs().fromNow()}</span>}
-          />
+          {comment.map((cmt: any) => {
+            return (
+              <Comment
+                key={cmt.id}
+                author={<a>{cmt.user.username}</a>}
+                avatar={
+                  <Avatar
+                    src="https://joeschmoe.io/api/v1/random"
+                    alt={cmt.user.username}
+                  />
+                }
+                content={<p>{cmt.comment}</p>}
+                datetime={<span>{dayjs().fromNow()}</span>}
+              />
+            );
+          })}
         </div>
         <div className="sticky w-full max-w-screen-sm m-auto bottom-0 left-0 pl-6 py-2 border-t-2">
-          <Form layout="inline" className="justify-between">
-            <Form.Item className="flex-1">
+          <Form layout="inline" onFinish={onFinish} className="justify-between">
+            <Form.Item className="flex-1" name="comment">
               <Input bordered={false} placeholder="Write a comment..." />
             </Form.Item>
             <Form.Item>
--- src/common/service/comment.ts
+++ src/common/service/comment.ts
@@ -1,15 +1,24 @@
 import { axiosInstance } from "./axios";
 
+interface IComment {
+  comment: string;
+}
 
 const CommentService = {
-    async list() {
-        const { data } = await axiosInstance.get("/comments");
+  async list(postId: string) {
+    const { data } = await axiosInstance.get(`/comment/${postId}`);
 
-        const $data: Array<any> = data;
+    const $data: Array<any> = data;
 
-        return $data;
-    }
+    return $data;
+  },
+  async send(payload: IComment, postId: string) {
+    const { data } = await axiosInstance.post(`/comment/${postId}`, payload);
 
-    
+    const $data: any = data;
 
-}
+    return $data;
+  },
+};
+
+export default CommentService;
